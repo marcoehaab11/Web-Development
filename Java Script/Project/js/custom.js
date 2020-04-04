4ad071991e5615872618ff39efe47ce5d5ca2098
Marco Ehab <57108557+marcoehab11@users.noreply.github.com>
2020-04-04 18:09:35 +0200

    Add files via upload

--- Java Script/Project/js/custom.js
+++ Java Script/Project/js/custom.js
@@ -38,15 +38,62 @@ $(function(){
 	// Trigger Bxslider
 
      $('.slider').bxSlider();
-    
-     // Smoth Scroll To Div
 
+     //Smoth Scroll to Div
      $('.ser').click(function(){
-     	$('html','body').animate({
-     		 scrollTop: $('#' + $(this).data('value')).offset().top
+     	$(("html,boy")).animate({
+     		scrollTop:$('#servies').offset().top
      	},1000);
-     })
+     });
+     $('.team').click(function(){
+     	$(("html,boy")).animate({
+     		scrollTop:$('#porta').offset().top
+     	},1000);
+    });
+          $('.test').click(function(){
+     	$(("html,boy")).animate({
+     		scrollTop:$('#test').offset().top
+     	},1000);
+    });
+          $('.port').click(function(){
+     	$(("html,boy")).animate({
+     		scrollTop:$('#Project').offset().top
+     	},1000);
+    });
 
+    //Our Auto Slider Code
+
+    (function autoslider(){
+    	$('.sliderr .active').each(function(){
+    		if(!$(this).is(':last-child')){
+    			$(this).delay(3000).fadeOut(1000,function(){
+    				$(this).removeClass('active').next().addClass('active').fadeIn();
+    				autoslider();
+    			});
+    		} else{
+    			$(this).delay(3000).fadeOut(1000,function(){
+    				$(this).removeClass('active');
+    				$('.sliderr div').eq(0).addClass('active').fadeIn();
+    				autoslider();
+    			})
+    		}
+    	});
+    }());
+
+   $('#Container').mixItUp();
+
+   // Adjust shuffle Links
+
+   $('.ourproject li').click(function(){
+   	$(this).addClass('active2').siblings().removeClass('active2');
+   });
+
+   // Triggle Nice Scroll
+   $('html').niceScroll({
+   	cursorcolor:'#50c29e',
+   	cursorborder: "1px solid #50c29e",
+   	cursorwidth: "4px"
+   });
 
 });
  
